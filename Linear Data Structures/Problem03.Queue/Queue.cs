bf4d2c55a553002933df82d5cbb34f9184529dd3
Stoyko Kolev <stoikokolev@gmail.com>
2020-09-28 20:13:41 +0300

    Queue Implementation

--- Linear Data Structures/Problem03.Queue/Queue.cs
+++ Linear Data Structures/Problem03.Queue/Queue.cs
@@ -8,34 +8,89 @@
     {
         private Node<T> _head;
 
+        public Queue()
+        {
+            this._head = null;
+            this.Count = 0;
+        }
+
+        public Queue(Node<T> node)
+        {
+            this._head = node;
+            this.Count = 1;
+        }
+
         public int Count { get; private set; }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var current = this._head;
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+
+            return false;
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            this.CheckIfEmpty();
+            var toReturn = this._head.Value;
+            this._head = this._head.Next;
+            this.Count--;
+            return toReturn;
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            var toInsert = new Node<T>(item);
+            if (this.Count == 0)
+            {
+                this._head = toInsert;
+            }
+            else
+            {
+                var current = this._head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = toInsert;
+            }
+            this.Count++;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.CheckIfEmpty();
+            return this._head.Value;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = this._head;
+            while (current.Next != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
-            => throw new NotImplementedException();
+            => this.GetEnumerator();
+
+        private void CheckIfEmpty()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
     }
 }

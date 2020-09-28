644d6fe01f2179687fb93d8f2d4cdaba30cc3e18
Stoyko Kolev <stoikokolev@gmail.com>
2020-09-28 20:05:31 +0300

    Stack Implementation

--- Linear Data Structures/Problem02.Stack/Stack.cs
+++ Linear Data Structures/Problem02.Stack/Stack.cs
@@ -8,34 +8,78 @@
     {
         private Node<T> _top;
 
+        public Stack()
+        {
+            this._top = null;
+            this.Count = 0;
+        }
+
+        public Stack(Node<T> node)
+        {
+            this._top = node;
+            this.Count = 1;
+        }
+
         public int Count { get; private set; }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            this.CheckIfEmpty();
+            var current = this._top;
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+
+            return false;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.CheckIfEmpty();
+            return this._top.Value;
         }
-
+        
         public T Pop()
         {
-            throw new NotImplementedException();
+            this.CheckIfEmpty();
+            var toReturn = this._top.Value;
+            this._top = this._top.Next;
+            this.Count--;
+            return toReturn;
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            var toInsert = new Node<T>(item);
+            toInsert.Next = this._top;
+            this._top = toInsert;
+            this.Count++;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = this._top;
+            while (current.Next!= null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator() 
-            => throw new NotImplementedException();
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+       
+        private void CheckIfEmpty()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
     }
 }

8b44e7bff92073758950ef41dc71fd510d0aa7f9
Stoyko Kolev <stoikokolev@gmail.com>
2020-09-29 12:52:47 +0300

    Doubly Linked List Implementation

--- Linear Data Structures - Excercise/02.DoublyLinkedList/DoublyLinkedList.cs
+++ Linear Data Structures - Excercise/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -6,48 +6,146 @@
 
     public class DoublyLinkedList<T> : IAbstractLinkedList<T>
     {
-        private Node<T> head;
+        private Node<T> _head;
+
+        private Node<T> _tail;
+
+        public DoublyLinkedList()
+        {
+            this._head = null;
+
+            this._tail = null;
+
+            this.Count = 0;
+        }
+
+        public DoublyLinkedList(Node<T> item)
+        {
+            this._head = item;
+
+            this._tail = item;
+
+            this.Count = 1;
+        }
 
         public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            var toInsert = new Node<T>(item);
+
+            if (this.Count == 0)
+            {
+                this._head = this._tail = toInsert;
+            }
+            else if (this.Count == 1)
+            {
+                this._head = toInsert;
+                this._head.Next = this._tail;
+                this._tail.Previous = this._head;
+            }
+            else
+            {
+                this._head.Previous = toInsert;
+                toInsert.Next = this._head;
+                this._head = toInsert;
+            }
+
+            this.Count++;
+
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            var toInsert = new Node<T>(item);
+
+            if (this.Count == 0)
+            {
+                this._head = this._tail = toInsert;
+            }
+            else if (this.Count == 1)
+            {
+                this._head.Next = toInsert;
+                this._tail = toInsert;
+                this._tail.Previous = this._head;
+            }
+            else
+            {
+                this._tail.Next = toInsert;
+                toInsert.Previous = this._tail;
+                this._tail = toInsert;
+            }
+
+            this.Count++;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+            return this._head.Item;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+            return this._tail.Item;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+            var toReturn = this._head.Item;
+            if (this.Count == 1)
+            {
+                this._head = this._tail = null;
+            }
+            else
+            {
+                this._head = this._head.Next;
+                this._head.Previous = null;
+            }
+
+            this.Count--;
+            return toReturn;
+
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+            var toReturn = this._tail.Item;
+            if (this.Count == 1)
+            {
+                this._head = this._tail = null;
+            }
+            else
+            {
+                this._tail = this._tail.Previous;
+                this._tail.Next = null;
+            }
+
+            this.Count--;
+            return toReturn;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = this._head;
+
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private void EnsureNotEmpty()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+                throw new InvalidOperationException();
         }
+
     }
 }
--- Linear Data Structures - Excercise/02.DoublyLinkedList/Node.cs
+++ Linear Data Structures - Excercise/02.DoublyLinkedList/Node.cs
@@ -5,5 +5,21 @@
         public T Item { get; set; }
 
         public Node<T> Next { get; set; }
+
+        public Node<T> Previous { get; set; }
+
+        public Node()
+        {
+
+        }
+
+        public Node(T item, Node<T> next = null, Node<T> previous = null)
+        {
+            this.Item = item;
+
+            this.Next = next;
+
+            this.Previous = previous;
+        }
     }
 }

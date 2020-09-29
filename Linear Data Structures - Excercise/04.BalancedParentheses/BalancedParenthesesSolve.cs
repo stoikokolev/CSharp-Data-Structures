640c9cd99544fd594fdeb437d5d6b0535e7fd581
Stoyko Kolev <stoikokolev@gmail.com>
2020-09-29 16:54:20 +0300

    Balanced Parantheses Solved

--- Linear Data Structures - Excercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ Linear Data Structures - Excercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -7,7 +7,55 @@
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            var stack = new Stack<char>();
+            stack.Push(parentheses[0]);
+            for (int i = 1; i < parentheses.Length; i++)
+            {
+                char symbol = parentheses[i];
+                switch (symbol)
+                {
+                    case '[':
+                    case '{':
+                    case '(':
+                        stack.Push(symbol);
+                        break;
+                    case '}':
+                        if (stack.Count>0 && stack.Peek() == '{')
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            stack.Push(symbol);
+                        }
+                        break;
+                    case ']':
+                        if (stack.Count > 0 && stack.Peek() == '[')
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            stack.Push(symbol);
+                        }
+                        break;
+                    case ')':
+                        if (stack.Count > 0 && stack.Peek() == '(')
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            stack.Push(symbol);
+                        }
+                        break;
+                    default:
+                        stack.Push(symbol);
+                        break;
+                }
+            }
+
+            return stack.Count == 0;
         }
     }
 }

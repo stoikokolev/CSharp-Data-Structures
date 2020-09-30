7c82a86936ff28688eb8d0e1541e6e45b4427db3
Stoyko Kolev <stoikokolev@gmail.com>
2020-09-30 19:28:05 +0300

    Max Heap - passed tests with list implementation

--- Heaps and Binary Search Trees/02.MaxHeap/MaxHeap.cs
+++ Heaps and Binary Search Trees/02.MaxHeap/MaxHeap.cs
@@ -1,20 +1,30 @@
-﻿namespace _02.MaxHeap
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.MaxHeap
 {
     using System;
 
     public class MaxHeap<T> : IAbstractHeap<T>
         where T : IComparable<T>
     {
-        public int Size { get { throw new NotImplementedException(); } }
+        private List<T> _elements = new List<T>();
+
+        public int Size
+        {
+            get { return this._elements.Count; }
+        }
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            
+            this._elements.Add(element);
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this._elements.Sort();
+            return this._elements[_elements.Count-1];
         }
     }
 }

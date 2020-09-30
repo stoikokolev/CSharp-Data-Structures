c13e364deaae9c03b2311952170c70ab626c53b0
Stoyko Kolev <stoikokolev@gmail.com>
2020-09-30 19:56:33 +0300

    Max Heap Implemented

--- Heaps and Binary Search Trees/02.MaxHeap/MaxHeap.cs
+++ Heaps and Binary Search Trees/02.MaxHeap/MaxHeap.cs
@@ -8,23 +8,63 @@ namespace _02.MaxHeap
     public class MaxHeap<T> : IAbstractHeap<T>
         where T : IComparable<T>
     {
-        private List<T> _elements = new List<T>();
+        private List<T> _elements;
 
-        public int Size
+        public MaxHeap()
         {
-            get { return this._elements.Count; }
+            this._elements=new List<T>();
         }
+
+        public int Size => this._elements.Count;
 
         public void Add(T element)
         {
-            
             this._elements.Add(element);
+
+            this.HeapifyUp();
+        }
+
+        private void HeapifyUp()
+        {
+            int indexOfElement = this._elements.Count - 1;
+            int parentIndex = this.GetParentIndex(indexOfElement);
+
+            while (this.IndexIsValid(indexOfElement)
+                   && this.IsGreater(indexOfElement,parentIndex))
+            {
+                this.Swap(indexOfElement, parentIndex);
+
+                indexOfElement = parentIndex;
+                parentIndex = this.GetParentIndex(indexOfElement);
+            }
+            
         }
 
         public T Peek()
         {
-            this._elements.Sort();
-            return this._elements[_elements.Count-1];
+            return this._elements[0];
+        }
+
+        private void Swap(int indexOfElement, int parentIndex)
+        {
+            var temp = this._elements[indexOfElement];
+            this._elements[indexOfElement] = this._elements[parentIndex];
+            this._elements[parentIndex] = temp;
+        }
+
+        private bool IndexIsValid(int index)
+        {
+            return index > 0;
+        }
+
+        private int GetParentIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+
+        private bool IsGreater(int childIndex, int parenIndex)
+        {
+            return this._elements[childIndex].CompareTo(this._elements[parenIndex]) > 0;
         }
     }
 }
